44c3b5328b15641559a800f2d5038f6c68107313
AlpLab <alplabdevelop@gmail.com>
2023-03-19 16:06:25 +0200

    Added frontend and api volumes for hotreload

--- time-app/frontend/src/components/Time.vue
+++ time-app/frontend/src/components/Time.vue
@@ -30,7 +30,7 @@
   <div class="greetings">
     <h1 class="green">{{ currentTime }}</h1>
     <h3>Нажмите кнопку для сохранения в базе данных</h3>
-    <button @click="saveTime">Сохранить время</button>
+    <button @click="saveTime">Зберігти час!!!</button>
     <h3 v-if="savedTimes.length && showSavedTimes">
       Ранее сохраненные времена:
     </h3>
